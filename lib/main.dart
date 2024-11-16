7bd4defb381329205fd45d54acf9599b463ef658
Bruno Winter <brunohdwinter@gmail.com>
2024-11-16 10:11:50 -0400

    Criando projeto do 0, adicionando firebase auth, cadastro login e recuperacao de senha

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gerenciamento_veiculos/Screen/Login-Cadastro/login.dart';
+import 'package:gerenciamento_veiculos/Screen/home.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, AsyncSnapshot<User?> snapshot){
+          if(snapshot.hasData && snapshot.data != null){
+            return HomeScreen();
+          }else if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else{
+            return LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
